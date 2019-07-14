c1a2aca5870b1f93cb8aad4c88c877d19c16f4b9
KG <kg.vitaty14@gmail.com>
2019-07-14 17:20:00 +0900

    Update list and function

--- lib/main.dart
+++ lib/main.dart
@@ -3,146 +3,80 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
 
+//app entry point
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'my_alcohol_list',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.lightBlue,
       ),
-      home: MyHomePage(title: 'my_alcohol_list'),
+      home: TextFieldAlertDialog(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+//app entry point end
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class TextFieldAlertDialog extends StatelessWidget {
+  TextEditingController _textFieldController = TextEditingController();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  List<Map<String, dynamic>> data = [
+    { "id" : 1,  "content" : "Content 1" },
+    { "id" : 2,  "content" : "Content 2" },
+    { "id" : 3,  "content" : "Content 3" }
+  ];
 
-  final String title;
+  int _counter = 3;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  _displayDialog(BuildContext context) async {
+    print('_displaydialog');
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Add alcohol'),
+            content: TextField(
+              controller: _textFieldController,
+              autofocus: true,
+              decoration: InputDecoration(labelText: 'Bland Name', hintText: 'ex.hoge beer'),
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text('OK'),
+                onPressed: () => _submission(_textFieldController.text),
+              )
+            ],
+          );
+        });
+  }
+  void _submission(String inputText) {
+    print('_submission');
+    _counter++;
+    data.add({ "id": _counter, "content": inputText});
   }
 
-  void _showDialog() {
-    setState(() {
-      showDialog(
-          context: context,
-          builder: (_) => AlertDialog(
-            title :Text("Hello !"),
-            content: Text("Welcome to Flutter World !"),
-          )
-      );
-    });
-  }
-
-//  @override
-//  Widget build(BuildContext context) {
-//    // This method is rerun every time setState is called, for instance as done
-//    // by the _incrementCounter method above.
-//    //
-//    // The Flutter framework has been optimized to make rerunning build methods
-//    // fast, so that you can just rebuild anything that needs updating rather
-//    // than having to individually change instances of widgets.
-//    return Scaffold(
-//      appBar: AppBar(
-//        // Here we take the value from the MyHomePage object that was created by
-//        // the App.build method, and use it to set our appbar title.
-//        title: Text(widget.title),
-//      ),
-//      body: Center(
-//        // Center is a layout widget. It takes a single child and positions it
-//        // in the middle of the parent.
-//        child: Column(
-//          // Column is also layout widget. It takes a list of children and
-//          // arranges them vertically. By default, it sizes itself to fit its
-//          // children horizontally, and tries to be as tall as its parent.
-//          //
-//          // Invoke "debug painting" (press "p" in the console, choose the
-//          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//          // to see the wireframe for each widget.
-//          //
-//          // Column has various properties to control how it sizes itself and
-//          // how it positions its children. Here we use mainAxisAlignment to
-//          // center the children vertically; the main axis here is the vertical
-//          // axis because Columns are vertical (the cross axis would be
-//          // horizontal).
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          children: <Widget>[
-//            Text(
-//              'You have pushed the button this many times:',
-//            ),
-//            Text(
-//              '$_counter',
-//              style: Theme.of(context).textTheme.display1,
-//            ),
-//          ],
-//        ),
-//      ),
-//      floatingActionButton: FloatingActionButton(
-//        onPressed: _incrementCounter,
-//        tooltip: 'Increment',
-//        child: Icon(Icons.add),
-//      ), // This trailing comma makes auto-formatting nicer for build methods.
-//    );
-//  }
 
   @override
   Widget build(BuildContext context) {
-    const data = [
-      "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県",
-    ];
+    debugPrint('debug');
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('my_alcohol_list'),
       ),
       body: ListView.builder(
         itemCount: data.length,
-        itemBuilder: (context, int index) {
+        itemBuilder: (int, context) {
           return Padding(
               padding: EdgeInsets.all(8.0),
-              child: Text(
-                data[index],
-              ));
+              child: Text(data[0].toString()),
+          );
         },
       ),
     floatingActionButton: FloatingActionButton(
-        onPressed: _showDialog,
+        onPressed: () => _displayDialog(context),
         tooltip: 'Add',
         child: Icon(Icons.add),
       ),
